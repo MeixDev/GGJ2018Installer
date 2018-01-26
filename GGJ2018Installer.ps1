2d7c9ca524977300c09ab19810282959b68a8eef
MeixDev <main.meixdev@gmail.com>
2018-01-26 20:18:00 +0100

    Added LibFreetype 2.7.1, GLFW 3.2.1 (WIN32) and SFML 2.4.2. Also added PowerShell Script. This repo will be deleted at the end of the Jam

---
+++ GGJ2018Installer.ps1
@@ -0,0 +1,4 @@
+Write-Host "Installing GLFW 3.2.1, LibFreeType v2.7.1 and SFML 2.4.2 at the root of your C: drive."
+mv "./freetype-windows-binaries-2.7.1" C:/
+mv "./glfw-3.2.1.bin.WIN32" C:/
+mv "./SFML-2.4.2" C:/
